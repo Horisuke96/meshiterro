2465f0fbe4a14f81906cbfafdcc802b863984d35
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-05-12 07:00:12 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0a1bbd66ccee99a06e74b21ef4b24ea280e6d5ee2eb4bb4c2b1af89543a53332846f32d6a15a916b62ac4f28ed56be6c084475df5c5905451a4dd836c9c58fb5'
